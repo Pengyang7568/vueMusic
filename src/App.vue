cbcae9399e2c0129d790a575691fccf6bca03364
fabrice <pengyang7568@gmail.com>
2017-07-11 18:06:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+@import "common/sass/variable.scss";
+#app {
+  color: $color-theme;
+}
+
+</style>
